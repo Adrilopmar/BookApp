e8b54de94092505caafd27db6fc79af954da43ef
Adrià <adria.lop.mar@gmail.com>
2022-11-24 18:51:37 +0100

    added router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardBooksComponent } from './components/dashboarBooks/dashboard-books.component';
+import { SingleBookComponent } from './components/single-book/single-book.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:DashboardBooksComponent},
+  {path:'book/:isbn', component: SingleBookComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardBooksComponent } from './components/dashboarBooks/dashboard-books.component';
+import { SingleBookComponent } from './components/single-book/single-book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DashboardBooksComponent,
+    SingleBookComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
