67535e21f1add1df633c15e7a4d3a710fe4ca4be
Adrià <adria.lop.mar@gmail.com>
2022-11-25 00:39:15 +0100

    created search svc

--- src/app/services/books-service.service.ts
+++ src/app/services/books-service.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Book } from '../classes/books';
-import { Observable, of } from 'rxjs';
+import { Observable} from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +11,22 @@ export class BooksService {
 
   constructor( private http:HttpClient) { }
 
+  allBooks:any;
+  filteredBooks:any;
 
-getBooks():Observable<Book>{
-  return this.http.get<Book>('https://api.angular.schule/books')
+ getBooks():Observable<Book>{
+  let getAllbooks =  this.http.get<Book>('https://api.angular.schule/books')
+ getAllbooks.subscribe(books =>{ this.allBooks = books, this.filteredBooks=books})
+ 
+  return  getAllbooks
 }
 getBookIsbn(isbn:string | null):Observable<Book>{
   return  this.http.get<Book>(`https://api.angular.schule/books/${isbn}`)
 }
+ searchBooks(input:string){
+  this.filteredBooks = this.allBooks
+  this.filteredBooks = this.filteredBooks.filter((data:Book)=> data.title.trim().toLocaleLowerCase().includes(input.trim().toLocaleLowerCase()))
+}
+
 
 }
